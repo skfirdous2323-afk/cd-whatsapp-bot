fdb5ed84965048948e749fc7f08c711b83076d49
Sk Firdous <skfirdous2323@gmaill.com>
2026-07-16 04:37:41 +0530

    Disable business hours for testing

--- utils/businessHours.js
+++ utils/businessHours.js
@@ -1,21 +1,3 @@
 export function isClinicOpen() {
-  const now = new Date();
-
-  // India Time
-  const indiaTime = new Date(
-    now.toLocaleString("en-US", {
-      timeZone: "Asia/Kolkata",
-    })
-  );
-
-  const day = indiaTime.getDay(); // 0 = Sunday
-  const hour = indiaTime.getHours();
-
-  // Sunday Closed
-  if (day === 0) {
-    return false;
-  }
-
-  // Open: 9 AM - 6 PM
-  return hour >= 9 && hour < 18;
+  return true;
 }
